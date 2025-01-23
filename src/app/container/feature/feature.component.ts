ea93b3ad14501c1ab2d087ab9c0c778491a66a39
Harshinis06 <harshini20010206@gmail.com>
2025-01-23 16:31:38 +0530

    Add files via upload

---
+++ src/app/container/feature/feature.component.ts
@@ -0,0 +1,12 @@
+import { Component, ContentChild, contentChild, ElementRef } from '@angular/core';
+import { ContainerComponent } from '../container.component';
+
+@Component({
+  selector: 'app-feature',
+  imports: [],
+  templateUrl: './feature.component.html',
+  styleUrl: './feature.component.css'
+})
+export class FeatureComponent {
+
+}
