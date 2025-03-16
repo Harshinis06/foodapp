9538a276ca9f39004b7e3db4189f54769dc4b867
Harshinis06 <harshini20010206@gmail.com>
2025-03-16 16:27:33 +0530

    Delete src directory

--- src/app/container/customDerictive/host-bind-hover.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HostBindHoverDirective } from './host-bind-hover.directive';
-
-describe('HostBindHoverDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HostBindHoverDirective();
-    expect(directive).toBeTruthy();
-  });
-});
